8a480436c34ee4631a2ef21cc53919c71ff8f787
Prithvi <pc257773@gmail.com>
2026-05-13 15:40:59 +0530

    New

--- Practice/19_Multithreading.py
+++ Practice/19_Multithreading.py
@@ -1,4 +1,5 @@
 from time import perf_counter , sleep
+import threading
 
 
 #  Method 1 to find factorial
@@ -63,6 +64,50 @@ end_time3 = perf_counter()
 total_time3 = end_time3 - start_time3
 
 print(f"Total time taken for hello function is {total_time2}")
+
+
+
+#  Multithreading
+
+# this takes 3 seconds 
+
+start_time4 = perf_counter()
+t1 = threading.Thread(target= hello)
+t1.start()
+t1.join()
+t2 = threading.Thread(target= hello)
+t2.start()
+t2.join()
+t3 = threading.Thread(target = hello)
+t3.start()
+t3.join()
+end_time4 = perf_counter()
+total_time4 = end_time4 - start_time4
+
+print(f"Total time taken for hello function is {total_time4}")
+
+
+#  this takes 1 second
+
+start_time5 = perf_counter()
+t1 = threading.Thread(target= hello)
+t2 = threading.Thread(target= hello)
+t3 = threading.Thread(target = hello)
+t1.start()
+t2.start()
+t3.start()
+t1.join()
+t2.join()
+t3.join()
+end_time5 = perf_counter()
+total_time5 = end_time5 - start_time5
+
+print(f"Total time taken for hello function is {total_time5}")
+
+
+
+
+
 
 
 
